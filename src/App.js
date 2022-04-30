58664579b4dbefbad4d766c4629f82922cc2ed47
xanxit <sanchit.hajela@yojak.in>
2022-04-30 23:08:43 +0530

    added product page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { Routes, Route} from "react-router-dom";
 import Home from "./components/Home";
+import Products from "./components/Products";
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
         {/* <Route path="about" element={<About />} /> */}
       </Routes>
       </div>
