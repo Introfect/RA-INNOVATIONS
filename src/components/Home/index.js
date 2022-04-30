676a28d98eb2eb9981893db79df892b6dbafe9e5
xanxit <sanchit.hajela@yojak.in>
2022-04-30 23:49:34 +0530

    mapping done

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,17 @@
 import Footer from "../Footer";
 import Contributors from "./Contributors";
 import HeroComponent from "./HeroComponent";
-import ProductInfo from "./ProductInfo";
 import ResearchWork from "./ResearchWork";
+import { NavBar } from "../NavBar";
+import { ProductPage } from "./ProductComponent";
 
 const Home = () => {
   return (
     <div>
+      <NavBar />
       <HeroComponent />
       <h1 className="text-center md:py-8 text-gray-600 md:text-4xl text-2xl"> Our Products</h1>
-      <ProductInfo/>
+      <ProductPage/>
       <h1 className="text-center md:py-8 text-gray-600 md:text-4xl text-2xl"> Our Research Work</h1>
       <ResearchWork/>
       <h1 className="text-center md:py-8 text-gray-600 md:text-4xl text-2xl"> Contributors</h1>
