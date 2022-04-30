fe12cea7699b4d12e8329c4ae350f026d9f27bf3
Aayushman03 <aayushmanpratap333@gmail.com>
2022-05-01 00:22:48 +0530

    navbar fixed

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,23 @@
-import Footer from "../Footer";
+
+
+
+
+
+
+
+
+
+
+
+
 import Contributors from "./Contributors";
 import HeroComponent from "./HeroComponent";
 import ResearchWork from "./ResearchWork";
-import { NavBar } from "../NavBar";
 import { ProductPage } from "./ProductComponent";
 
 const Home = () => {
   return (
     <div>
-      <NavBar />
       <HeroComponent />
       <h1 className="text-center md:py-8 text-gray-600 md:text-4xl text-2xl"> Our Products</h1>
       <ProductPage/>
@@ -16,7 +25,6 @@ const Home = () => {
       <ResearchWork/>
       <h1 className="text-center md:py-8 text-gray-600 md:text-4xl text-2xl"> Contributors</h1>
       <Contributors/>
-      <Footer/>
     </div>
   );
 };
